21c8d1a3850905ac7b6728781de8d7c883289dae
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-02-10 12:25:28 +0300

    Updated code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,11 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-
-  },[])
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500)
+    
+    return () => {
+      clearTimeout(identifier)
+    }
+  }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
